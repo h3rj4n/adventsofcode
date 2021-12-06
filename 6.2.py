cdc104d79d1389fa0bb61f2bcd58f01f65fb837e
Herjan van Eijk <herjan.van.eijk@ndcmediagroep.nl>
2021-12-06 19:02:31 +0100

    Solved 6.x puzzles

--- 6.2.py
+++ 6.2.py
@@ -1,29 +1,34 @@
+from collections import defaultdict
+
 # Import the data
 f = open("6_data.txt")
 
-fishes = f.readline().rstrip('\n').split(',')
-fishes = list(map(lambda x: int(x), fishes))
+data = f.readline().rstrip('\n').split(',')
+fishes = defaultdict(int)
+
+for i in data:
+    fishes[int(i)] += 1
 
 days = 256
 
-for x in range(0, days):
-    newlyBorns = 0
-    # Loop trough all the fishes.
-    for index, fish in enumerate(fishes):
-        if fish == 0:
-            # Rest the timer
-            fishes[index] = 6
-            # Create a new fish.
-            newlyBorns += 1
+for _ in range(days):
+    # Create a new dictionary to copy the values.
+    clone_fishes = defaultdict(int)
+
+    for n in fishes:
+        # Create a new fish.
+        if n == 0:
+            # Reset the current fish.
+            clone_fishes[6] += fishes[n]
+            clone_fishes[8] = fishes[n]
         else:
-            # Decrease on of the fish.
-            fishes[index] = fish - 1
-    for i in range(0, newlyBorns):
-        fishes.append(8)
+            clone_fishes[n - 1] += fishes[n]
 
-print('asnwer: ', len(fishes))
+    fishes = clone_fishes
 
-# 553267 (too high)
-# 231817 (too low)
-# 333438 (too low)
-# 389726
+ans = 0
+for n in fishes:
+    ans += fishes[n]
+print('answer: ', ans)
+
+# 1743335992042
