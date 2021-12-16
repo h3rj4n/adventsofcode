2ffe5a47780e075ca41fb18c75a45c21860abdf2
Herjan van Eijk <git@f28.nl>
2021-12-16 21:51:32 +0100

    Cleanup some code

--- day_16/16.1.py
+++ day_16/16.1.py
@@ -10,20 +10,14 @@ with open('16_data.txt') as f:
 ans = 0
 
 
-def processBit(b, cont=True, type=None, size=None, depth=0):
+def processBit(b, depth=0):
     global ans
-    prefix = ''.join(['\t'] * depth)
-    depth += 1
 
-    # print(prefix, 'input: ', b)
     version = int(b[:3], 2)
     # not 4 = operator | 4 = literal value
     typeId = int(b[3:6], 2)
 
     ans += version
-
-    # print(prefix, 'packet version: ', version)
-    # print(prefix, 'packet type id: ', typeId)
 
     # For now, skip litteral data.
     if typeId == 4:
@@ -32,7 +26,6 @@ def processBit(b, cont=True, type=None, size=None, depth=0):
         numbers = wrap(d, 5)
         for n in numbers:
             rm += 5
-            print(prefix, 'number: ', int(n[1:], 2))
 
             if int(n[0]) == 0:
                 break
@@ -42,11 +35,8 @@ def processBit(b, cont=True, type=None, size=None, depth=0):
     lengthTypeId = int(b[6:7], 2)
     length = 15 if lengthTypeId == 0 else 11
 
-    # print(prefix, 'lenght type id: ', lengthTypeId, ' length: ', length)
-
     # Read the next part based
     subPacks = int(b[7:7+length], 2)
-    # print(prefix, 'subPacks: ', subPacks)
 
     # We've established some values and stats. remove this from the bit string.
     b = b[7+length:]
@@ -61,24 +51,8 @@ def processBit(b, cont=True, type=None, size=None, depth=0):
         actualData = b[:subPacks]
 
         while len(actualData) > 0:
-            # print(prefix, actualData)
             actualData = processBit(actualData, cont=True, depth=depth)
 
-        # Loop trough the data, grep the next 11 bits? until
-        # last group is found.
-        # items = wrap(actualData, 11)
-        # for i, d in enumerate(items):
-        #     lastBit = False
-        #
-        #     if int(d[:1]) == 0 and len(items) > i + 1:
-        #         # Found the last group.
-        #         lastBit = True
-        #         d += items[i+1]
-        #
-        #     # processBit(d, cont=True)
-        #
-        #     if lastBit:
-        #         break
         # Remove the part we've just processed.
         b = b[subPacks:]
 
@@ -95,8 +69,3 @@ for b in data:
     processBit(b)
 
 print(ans)
-
-# 8A004A801A8002F478 represents an operator packet (version 4) which
-# contains an operator packet (version 1) which contains an operator packet
-# (version 5) which contains a literal value (version 6); this packet has
-# a version sum of 16.
