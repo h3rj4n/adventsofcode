45890decc6851d3efa874f465a583c28c071c515
Herjan van Eijk <herjan.van.eijk@ndcmediagroep.nl>
2022-12-14 09:33:05 +0100

    Solved puzzles of day 4

--- day_4/4_2.py
+++ day_4/4_2.py
@@ -4,7 +4,7 @@ with open("4_data.txt") as f:
     for line in f:
         [aa, ab], [ba, bb] = sorted([[ int(x) for x in i.split('-')] for i in line.strip().split(',')], key=lambda x: x[0])
 
-        if aa == ba and ab <= bb or aa <= ba and ab >= bb:
+        if ba <= ab:
             answer += 1
 
 print(answer)
