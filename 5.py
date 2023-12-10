c51c7ab7f43782b24f2445f215b01a5d61d5fa74
Herjan van Eijk <herjan.van.eijk@ndcmediagroep.nl>
2023-12-10 09:57:28 +0100

    Trying solution for 5.2 [WIP]

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 from enum import Enum
-from itertools import chain
 import re
+import pprint
 
 class ToMap(Enum):
     SEED = 'seeds'
@@ -47,7 +47,7 @@ with open("5_data.txt") as f:
 
         if current_step not in map_values:
             continue
-        
+
         data = [int(n) for n in re.findall(r"([0-9]+)", line)]
         if data:
             l = [s[map_values[current_step]] for s in build]
@@ -80,36 +80,110 @@ with open("5_data.txt") as f:
             # Extract all the seeds
             m = re.findall(r"([0-9]+)", line)
             seeds = [int(n) for n in m]
-            if len(seeds) <= 0:
-                continue
-            for seed_id in chain(range(seeds[0], seeds[0] + seeds[1]), range(seeds[2], seeds[2] + seeds[3])):
+            for i in range(0, int(len(seeds) / 2)):
                 s = {}
-                # @todo Default to own ID on the entire list?
                 for search in ToMap:
-                    s[search] = seed_id
+                    s[search.value] = [seeds[i * 2], seeds[i * 2] + seeds[(i * 2) + 1]]
                 build.append(s)
-            continue
+            #     print(seeds[i * 2], seeds[(i * 2) + 1])
+            #     print(i)
+            # print(seeds)
+#             if len(seeds) <= 0:
+#                 continue
+#             for seed_id in chain(range(seeds[0], seeds[0] + seeds[1]), range(seeds[2], seeds[2] + seeds[3])):
+#                 s = {}
+#                 # @todo Default to own ID on the entire list?
+#                 for search in ToMap:
+#                     s[search] = seed_id
+#                 build.append(s)
+#             continue
 
         if current_step not in map_values:
             continue
-        
+
         data = [int(n) for n in re.findall(r"([0-9]+)", line)]
         if data:
-            l = [s[map_values[current_step]] for s in build]
+            print(data)
+            dest_range = [data[0], data[0] + data[2]]
+            source_range = [data[1], data[1] + data[2]]
+            print('line: ', source_range, dest_range, current_step.value)
+
+            l = [s[map_values[current_step].value] for s in build]
             for index, number in enumerate(l):
-                if int(data[1]) <= number <= int(data[1]) + int(data[2]):
-                    # print(data[0] + (int(data[1]) - number))
-                    build[index][current_step] = data[0] + (number - int(data[1]))
+                print(map_values[current_step].value, number, source_range)
+                c = build[index].copy()
+                # Left: Original source range is bigger, we need to split up.
+                if number[0] < source_range[0] and number[1] > source_range[0]:
+                    count = (source_range[0] - 1) - number[0]
+                    print("count left", count)
+                    value = [number[0], source_range[0] - 1] # Create new object.
+                    c[map_values[current_step].value] = value
                     found = False
                     for search in ToMap:
                         if found:
-                            build[index][search] = data[0] + (number - int(data[1]))
+                            c[search.value] = value
+                        else:
+                            found = search == current_step
+                            if found is False:
+                                c[search.value][1] = c[search.value][0] + count
+                    print('append', current_step.value, value)
+                    # print(c)
+                    build.append(c)
+                    build[index][current_step.value] = [source_range[0], number[1]]
+                    build[index][map_values[current_step].value] = [source_range[0], number[1]]
+                    number[0] = source_range[0]
+                    # [source_range[0], number[1]] # Overwrite current object.
+
+                # Right: Original source range is bigger, we need to split up.
+                if number[1] > source_range[1] and number[0] < source_range[1]:
+                    count = number[1] - (source_range[1] + 1)
+                    print("count right", count)
+                    value = [source_range[1] + 1, number[1]] # Create new object.
+                    c[map_values[current_step].value] = value
+                    found = False
+                    for search in ToMap:
+                        if found:
+                            c[search.value] = value
+                        else:
+                            found = search == current_step
+                            if found is False:
+                                c[search.value][0] = c[search.value][1] - count
+                    # print('t', map_values[current_step].value)
+                    print('append', current_step.value, value)
+                    # print(c)
+                    build.append(c)
+                    build[index][current_step.value] = [number[0], source_range[1]]
+                    build[index][map_values[current_step].value] = [number[0], source_range[1]]
+                    number[1] = source_range[1]
+
+                    # [source_range[1] + 1, number[1]] # Create new object
+                    # [number[0], source_range[1]] # Overwrite current object.
+
+                # Now we have an object that fits the dest_range?
+                if number[0] >= source_range[0] and number[1] <= source_range[1]:
+                    value =  [dest_range[0] + number[0] - source_range[0], dest_range[1] + number[1] - source_range[1]]
+                    build[index][current_step.value] = value
+                # print(number)
+#                 if int(data[1]) <= number <= int(data[1]) + int(data[2]):
+#                     # print(data[0] + (int(data[1]) - number))
+#                     build[index][current_step] = data[0] + (number - int(data[1]))
+                    found = False
+                    for search in ToMap:
+                        if found:
+                            build[index][search.value] = value
                         else:
                             found = search == current_step
 
-print(build)
+                    print("match", value)
+            print("\n")
 
-l = [s[ToMap.HUMIDITY_TO_LOCATION] for s in build]
-print("Answer 2:", min(l))
-            
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(build)
+
+l = [s[ToMap.HUMIDITY_TO_LOCATION.value] for s in build]
+pp.pprint(l)
+
+# l = [s[ToMap.HUMIDITY_TO_LOCATION] for s in build]
+# print("Answer 2:", min(l))
+
 
